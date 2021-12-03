16a2f2ecc265e2cfb4cb7c37f9a3fbd132dc5a6b
Zanco1993 <zanconatofabio@gmail.com>
2021-12-03 16:09:20 +0100

    2° metodo con risultato fuori dal ciclo

--- js/script.js
+++ js/script.js
@@ -6,6 +6,32 @@ let age = parseInt(prompt("Qual è la tua età?"));
 console.log(numeroKm);
 console.log(age);
 
+// 1° metodo
+
+// let prezzoBiglietto = 0.21 * numeroKm;
+// const scontoMinorenne = prezzoBiglietto * 0.2;
+// const scontoOver = prezzoBiglietto * 0.4;
+// let prezzoFinaleBiglietto;
+
+// if (age < 18) {
+//     prezzoFinaleBiglietto = prezzoBiglietto - scontoMinorenne;
+//     prezzoFinaleBiglietto = prezzoFinaleBiglietto.toFixed(2);
+//     biglietto.innerHTML = `Il costo del biglietto se sei minorenne è di ${prezzoFinaleBiglietto}€`;
+
+// } else if (age > 65) {
+//     prezzoFinaleBiglietto = prezzoBiglietto - scontoOver;
+//     prezzoFinaleBiglietto = prezzoFinaleBiglietto.toFixed(2);
+//     biglietto.innerHTML = `Il costo del biglietto se sei over 65 è di ${prezzoFinaleBiglietto}€`;
+
+// } else {
+//     prezzoFinaleBiglietto = prezzoBiglietto
+//     prezzoFinaleBiglietto = prezzoFinaleBiglietto.toFixed(2);
+//     biglietto.innerHTML = `Il costo del tuo biglietto è di ${prezzoFinaleBiglietto}€`;
+// }
+
+
+// secondo metodo
+
 let prezzoBiglietto = 0.21 * numeroKm;
 const scontoMinorenne = prezzoBiglietto * 0.2;
 const scontoOver = prezzoBiglietto * 0.4;
@@ -13,17 +39,13 @@ let prezzoFinaleBiglietto;
 
 if (age < 18) {
     prezzoFinaleBiglietto = prezzoBiglietto - scontoMinorenne;
-    prezzoFinaleBiglietto = prezzoFinaleBiglietto.toFixed(2);
-    biglietto.innerHTML = `Il costo del biglietto se sei minorenne è di ${prezzoFinaleBiglietto}€`;
-
-} else if (age > 65) {
+} else if (age > 65){
     prezzoFinaleBiglietto = prezzoBiglietto - scontoOver;
-    prezzoFinaleBiglietto = prezzoFinaleBiglietto.toFixed(2);
-    biglietto.innerHTML = `Il costo del biglietto se sei over 65 è di ${prezzoFinaleBiglietto}€`;
-
 } else {
-    prezzoFinaleBiglietto = prezzoBiglietto
-    prezzoFinaleBiglietto = prezzoFinaleBiglietto.toFixed(2);
-    biglietto.innerHTML = `Il costo del tuo biglietto è di ${prezzoFinaleBiglietto}€`;
+    prezzoFinaleBiglietto = prezzoBiglietto;
 }
+
+prezzoFinaleBiglietto = prezzoFinaleBiglietto.toFixed(2);
+
+biglietto.innerHTML = `Il prezzo del biglietto è di ${prezzoFinaleBiglietto}€`;
 
